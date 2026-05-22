5c2ad2611b36a7ec0aeb6eec230fb6dfe52c9b04
Neverrie <korobov.ka@students.dvfu.ru>
2026-05-22 13:40:20 +1000

    Удаление старой реализации и очистка проекта

--- backend/app/models.py
+++
@@ -1,5 +0,0 @@
-from typing import Literal
-
-ChatKind = Literal["lab3_chat", "lab2_pipeline", "general"]
-MessageRole = Literal["user", "assistant", "system"]
-ArtifactKind = Literal["dataset", "chart", "table", "report", "json", "trace", "image", "other"]
